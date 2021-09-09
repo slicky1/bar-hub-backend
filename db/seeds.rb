bab44bc786888c4fe3b2d548c2cc81f206440555
slicky1 <okunonke@gmail.com>
2021-09-08 23:55:35 -0400

    Init

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create!(name: 'Beer', image: 'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2021_18/3469828/210503-beer-stock-mn-1500.jpg')
+Category.create!(name: 'Wine', image: 'https://static.onecms.io/wp-content/uploads/sites/9/2020/08/11/best-affordable-foreign-wines-FT-BLOG0820.jpg')
+Category.create!(name: 'Vodka', image: 'https://www.thelist.com/img/gallery/when-you-drink-vodka-every-night-this-is-what-happens-to-your-body/intro-1580240935.jpg')
+Category.create!(name: 'Whiskey', image: 'https://cdn.britannica.com/71/192771-050-CEF9CEC3/Glass-scotch-whiskey-ice.jpg')
+Category.create!(name: 'Brandy', image: 'https://cf.ltkcdn.net/cocktails/images/orig/267128-2121x1414-brandy-and-fire.jpg')
