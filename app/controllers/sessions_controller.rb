b9a8b9f2a58214e82c8765447d40084d38fd43c2
slicky1 <okunonke@gmail.com>
2021-09-24 02:56:31 -0400

    sessions controller continues

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,18 @@
 class SessionsController < ApplicationController
     def create
-
+        customer = Customer.find_by(username: customer_params[:username])
+        session[:customer_id] = customer.id
+        render json: customer
     end
 
     def destroy
-        
+        session.delete :customer_id
+        render json: {"message": "delete route"}
+    end
+
+    private
+
+    def customer_params
+        params.permit(:username, :session)
     end
 end
