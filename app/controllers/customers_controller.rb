3904b720e7cfb8f1dfd8b9897ddde1212584ab55
slicky1 <okunonke@gmail.com>
2021-09-24 01:43:07 -0400

    sessions controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,17 @@
 class CustomersController < ApplicationController
 
+#    def index
+#         session[:id] = "id"
+#         cookies[:test] = "test"
+#         render json: { session: session, cookies: cookies}
+#    end
+
+#    def show
+#     byebug
+#     render json: { message: "show"}
+#    end
+    
+    
     def index
         @customers = Customer.all
         render json: @customers
@@ -10,20 +22,20 @@ class CustomersController < ApplicationController
         render json: @customer
     end 
 
-    # def create
-    #     @customer = Customer.new(customer_params)
-    #     if @customer.save
-    #         login(@customer)
-    #         render "api/customers/show"
-    #     else
-    #         render json: @customer.errors.full_messages, status: 422
-    #     end
-    # end
+    def create
+        @customer = Customer.new(customer_params)
+        if @customer.save
+            login(@customer)
+            render "api/customers/show"
+        else
+            render json: @customer.errors.full_messages, status: 422
+        end
+    end
 
-    # private
+    private
 
-    # def customer_params
-    #     params.require(:customer).permit(:email, :password_digest, :first_name, :last_name, :username)
-    # end
+    def customer_params
+        params.require(:customer).permit( :first_name, :last_name, :username, :email, :password_digest)
+    end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   resources :brands
   resources :categories
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
